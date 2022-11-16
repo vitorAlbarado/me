08a13d84dc417d0c7a5e7a04b68cff8732df984b
Vitor Albarado <vitor.jardina@gmail.com>
2022-11-16 12:32:17 -0400

    Add navbar component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,29 @@
+import {FaFileDownload} from 'react-icons/fa'
 import styles from './NavBar.module.css';
 import Typical  from 'react-typical';
+import { Link } from 'react-router-dom';
 
 export default function NavBar (){
   return (
     <header className={styles.header}>
-      <p className={styles.title}>{`<`}
+      <span className={styles.title}>{`<`}
         <Typical
           steps={['Vitor Albarado',10000,'',1000]}
           loop={Infinity}
           wrapper="p"
           />
           {`/>`}
-        </p>
+        </span>
       <div className={styles.navbar}>
         <nav >
           <ul>
             <li><a href="about">Sobre mim</a></li>
             <li><a href="expirience">Experiência</a></li>
-            <li><a href="projects">Projetos</a></li>
+            <li><Link to="/projects">Projetos</Link></li>
             <li><a href="contact">Contato</a></li>
           </ul>
         </nav>
-        <div className={styles.resume}><a href="../../public/assets/Vitor-Albarado-estagio.pdf" target="_blank" download>CV</a></div>
+        <div className={styles.resume}><FaFileDownload/><a href="../../public/assets/Vitor-Albarado-estagio.pdf" target="_blank" download>CV</a></div>
       </div>
     </header>
   );
