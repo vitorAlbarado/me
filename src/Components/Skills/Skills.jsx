7ff8e32f5d1e8a53af41ca0910f208c7220da899
Vitor Albarado <vitor.jardina@gmail.com>
2022-11-16 12:35:38 -0400

    add skills component

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,19 @@
+import {FaReact, FaSass, FaGitAlt,FaGithub, FaJsSquare,FaBootstrap} from 'react-icons/fa';
+import {SiTypescript} from 'react-icons/si'
+import styles from './Skills.module.css';
+export default function Skills(){
+  return(
+    <section className={styles.skills}>
+      <h2>Soft Skills </h2>
+      <div className={styles.skills__icons}>
+        <label><FaReact/> </label>
+        <label><FaJsSquare/></label>
+        <label><SiTypescript/></label>
+        <label><FaGitAlt/></label>
+        <label><FaGithub/></label>
+        <label><FaSass/></label>
+        <label><FaBootstrap/></label>
+      </div>
+    </section>
+  );
+}
